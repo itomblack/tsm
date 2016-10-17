2a188ebf02ee7efbf2f1bd5a8b02a99fe7de8073
Ian Black <ianblack@Admins-MacBook-Pro-7.local>
2016-10-17 17:35:12 +0100

    sliding

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,21 @@
 (function () { 'use strict';
 
+	var number = 4;
+	var numbPrev = 4;
+
 	//hide cheapest tooltip
 	$('#cheapest-tooltip').attr('class', 'js-hide-opacity');
 	//aniamte slider
 	$('#tooltip-load').attr('class', 'animated-slide');
+	//hightlight current day
+	$('#graph-4').attr('class', 'js-highlighted');
 
 
 	document.getElementById('card-loading').onclick = function(){
 
 		//hide spinners
-		document.getElementById('tooltip-load').classList.add("js-hide");
-		document.getElementById('card-loading').classList.add("js-hide");
+		$('#tooltip-load').attr("class", "js-hide");
+		$('#card-loading').attr("class", "js-hide-opacity");
 
 		// animate graph
 		$('[id^="graph-bar"]').each(function() {			
@@ -33,31 +38,67 @@
 		//apply dragable
 		$( "#tooltip-drag" ).draggable({
 		  axis: "x",
-		  containment: "parent"
+		  containment: "parent",
+		  drag: function( event, ui ) {
+		  var position = $( "#tooltip-drag" ).position();
+
+		  	if (position.left >= 0 && position.left <= 20) {
+		  		number = 1
+		  		highlight(number);
+		  	}
+		  	if (position.left >= 25 && position.left <= 60) {
+		  		number = 2
+		  		highlight(number);
+		  	}
+		  	if (position.left >= 70 && position.left <= 100) {
+		  		number = 3
+		  		highlight(number);
+		  	}
+		  	if (position.left >= 115 && position.left <= 150) {
+		  		number = 4
+		  		highlight(number);
+		  	}
+		  	if (position.left >= 165 && position.left <= 190) {
+		  		number = 5
+		  		highlight(number);
+		  	}
+		  	if (position.left >= 200 && position.left <= 235) {
+		  		number = 6
+		  		highlight(number);
+		  	}
+		  	if (position.left >= 240) {
+		  		number = 7
+		  		highlight(number);
+		  	}
+		  }
 		});
 		
 
 	}; //ENDS CARD LOADING CLICK
 
+	
+
+	var highlight = function(number) {
+		if (number != numbPrev) {
+
+			//get numbers of charts
+			var graphId = '#graph-' + number;
+			var graphPrevId = '#graph-' + numbPrev;
+
+			//highlight the right one
+			$(graphId).attr('class', 'js-highlighted');
+			$(graphPrevId).attr('class', '');
+			//reset previous chart
+			numbPrev = number;	
+			//show card loader briefly
+			$('#card-loading').attr('class', '');
+			setTimeout(function (){
+				$('#card-loading').attr('class', 'js-hide-opacity');
+			}, 300);
 
 
-	$('#graph-7').hover( function() {
-		// console.log($('#graph-7 g'));
-
-		// $('#graph-7 g').each(function() {	
-		// 	$(this).attr('fill', 'red');
-		// 	$(this).style('fill', 'red');
-
-		// })
-
-		// $('#graph-7 g rect').each(function() {	
-		// 	$(this).attr('fill', 'red');
-		// 	$(this).style('fill', 'red');
-		// })
-
-		// $('#graph-bar_6_').attr('fill', 'red').style('fill', 'red');
-
-	})
+		}
+	}
 
 
 
