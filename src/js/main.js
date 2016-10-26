e79b2837e94e853f2ec95c92ace461c3f0d03d60
itomblack <itomblack@hotmail.co.uk>
2016-10-26 17:47:05 +0100

    start days each site build

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,47 @@
-(function () { 'use strict';
+$( document ).ready(function() {
 
-  console.log('test');
+	var desData = [{  
+	    'id': '1',
+	    'date': '27',
+	    'day': 'Th',
+	    'price': '230'
+	},
+	{
+	    'id': '2',
+	    'date': '28',
+	    'day': 'Fr',
+	    'price': '330'
+	},
+	{
+	    'id': '3',
+	    'date': '29',
+	    'day': 'Sa',
+	    'price': '430'
+	},
+	{
+	    'id': '4',
+	    'date': '30',
+	    'day': 'Su',
+	    'price': '370'
+	},
+	{
+	    'id': '5',
+	    'date': '31',
+	    'day': 'Mo',
+	    'price': '500'
+	},
+	{
+	    'id': '6',
+	    'date': '1',
+	    'day': 'Tu',
+	    'price': '300'
+	},
+	{   
+	    'id': '7',
+	    'date': '2',
+	    'day': 'We',
+	    'price': '210'
+	}]
 
 
 
@@ -8,4 +49,4 @@
 
 
 
-}()); // end 'use strict'
+}); // end 'use strict'
